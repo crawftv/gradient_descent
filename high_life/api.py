163153a3a174e3265df5a0fdd18bce36c938405e
Crawford Collins <github@crawfordc.com>
2024-05-01 22:07:52 -0500

    integrated llama3-dolphin long context model, eliminating accumulating prompts and mistral usage

--- high_life/api.py
+++ high_life/api.py
@@ -1,5 +1,4 @@
 import os
-import re
 import sqlite3
 import uuid
 from urllib.parse import urlparse, urlunparse
@@ -8,21 +7,24 @@ from anthropic import Anthropic
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from llama_index.core import PromptTemplate
-from llama_index.core.schema import NodeWithScore, MetadataMode
+from llama_index.core import PromptTemplate, VectorStoreIndex
+from llama_index.core.schema import MetadataMode
 from llama_index.llms.ollama import Ollama
 from pydantic import BaseModel
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 
 from agent_splitter import get_nodes
+from prompts import accumulated_prompt
 from scraper import scrape_website
 from search import log_retrieval, retrieve_documents, gather_nodes_recursively
-from settings import logging_startup
+from settings import logging_startup, vector_store, storage_context
 
 logging_startup()
 app = FastAPI()
 app.mount("/src", StaticFiles(directory="src"), name="src")
+llm = Ollama(model="dolphin-llama3:8b", request_timeout=500, additional_kwargs={"num_ctx": 256_000}, temperature=0.1)
+simple_index = VectorStoreIndex.from_vector_store(vector_store, storage_context=storage_context)
 
 origins = [
     "http://localhost:8002",
@@ -93,50 +95,6 @@ prompt = PromptTemplate(""" You are the world's best recommendation bot designed
      Then, provide your final answer within <answer>Yes/No</answer> tags.
     """)
 
-accumulated_prompt = PromptTemplate("""You are an expert Q&A assistant, specializing in extracting and synthesizing the most relevant information from provided knowledge bases to answer user queries accurately and concisely.
-
-Your role is to carefully analyze the given documents, identify the passages that directly address the user's question, and present that information in a clear, coherent response. If the knowledge base contains a complete answer, quote or paraphrase the relevant text. If it only partially answers the query, summarize the key relevant points. And if the provided documents do not contain a suitable answer, simply inform the user that an adequate answer could not be found in the given knowledge base.
-
-Focus on extracting information solely from the provided documents, without adding any external knowledge or personal opinions. Aim to provide a response that fully addresses the query using only the most pertinent information from the knowledge base.
-    -----------------
-    KNOWLEDGE_BASE: {docs}
-    -----------------
-    QUERY: {query_str}
-    -----------------
-    INSTRUCTIONS:If the documents do not contain a suitable answer, simply respond: "i could not find a suitable answer".
-    Do NOT suggest a solution from your own knowledge.
-    Do NOT include phrases like 'Based on the provided documents, or 'According to the documents'. or  'based on the documents provided.'
-    Include the url from the document metadata.
-    ------------------
-    Before answering the question, please think about it step-by-step within a <thinking></thinking> xml tag.
-    Then, provide your final answer within <answer></answer> xml tag
-    In the thinking steps, discuss the relevance of the content without mentioning document numbers.
-    If you want to cite a source, consider using the author's name or the platform instead of the document number. For example: "According to @claire_rose on Instagram,..."
-    EXAMPLES of  THOUGHT PROCESS:
-    <examples>
-    <example 1>
-        Query: seafood restaurant in Paris?
-        <thinking> The query asks for seafood restaurant recommendations in Paris. The provided information includes several relevant suggestions, such as a highly-regarded fish restaurant where everything is prepared exquisitely, and a top seafood spot in the city. Other details discuss wine pairings with seafood in general, which is less directly relevant to the specific query. 
-        </thinking>
-        <answer>
-        [X] is a restaurant in Paris that features [a,b,c].
-        </answer>
-    </example 1>
-    <example 2>
-     Query: restaurant in Hong kong?
-     <thinking>
-     The query asks for any restaurants or dining spots in hong kong, mentioned in the documents,
-     The provided documents include a recipe from a chef, John Woo, who has a restaurant in hong kong called the Hangout. The Hangout specializes in Hong Kong cuisine.
-     The document also ...
-     </thinking>
-     <answer>
-     1. The Hangout. The Hangout run by John Woo specializes in Hong Kong cuisine.
-     2 ...
-     </answer>
-    </example 2>
-    </examples>
-    """)
-
 
 def log_filtering_responses(logging_id, query_str: str, model_resp: str):
     with sqlite3.connect('logs.sqlite3') as conn:
@@ -155,38 +113,23 @@ def log_final_responses(logging_id, query_str: str, model_resp: str):
 def master_query(query_str: str, logging_id) -> str:
     # sourcery skip: inline-immediately-returned-variable
     log_query(logging_id, query_str=query_str)
-    docs: list[NodeWithScore] = retrieve_documents(query_str=query_str)  # + vector_retrieved_docs2
+    docs = simple_index.as_retriever(similarity_top_k=20).retrieve(query_str)
     log_retrieval(logging_id, docs)
     texts = gather_nodes_recursively(docs)
-    _answers = []
-    model = Ollama(model="mistral", temperature=0, request_timeout=500, )
-    for nodes in texts.values():
-        doc = " ".join([node.get_content(metadata_mode=MetadataMode.LLM) for node in nodes])
-        resp = model.complete(prompt.format(query_str=query_str, doc=doc))
-        log_filtering_responses(logging_id, doc, resp.text)
-        _answers.extend(
-            nodes
-            for i in re.finditer(
-                r"<answer>(?P<answer>.*)</answer>", resp.text.lower()
-            )
-            if "yes" in i.groupdict()["answer"]
-        )
-    if not _answers:
-        return "I could not find a suitable answer"
     answer_content = "<documents>"
-    for index, node_list in enumerate(_answers):
+    for index, node_list in enumerate(texts.values()):
         _answer_content = f"\n<document {index}:\n"
+        doc = " ".join([node.get_content(metadata_mode=MetadataMode.NONE) for node in node_list])
         for node in node_list:
-            _answer_content += f" {node.get_content(metadata_mode=MetadataMode.LLM)}"
+            _answer_content += f" {node.get_content(metadata_mode=MetadataMode.NONE)}"
 
         answer_content += f"{_answer_content}\n</document {index}>"
-
     answer_content += "\n</documents>"
     final_prompt = accumulated_prompt.format(
         query_str=query_str,
         docs=answer_content
     )
-    final_response = Ollama(model="mistral", request_timeout=500).complete(final_prompt).text
+    final_response = llm.complete(final_prompt).text
     # final_response = anthropic_call(final_prompt)
     log_final_responses(logging_id, final_prompt, final_response)
     xml = BeautifulSoup(f"<response>{final_response}</response>", 'lxml-xml')
